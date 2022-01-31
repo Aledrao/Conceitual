5dfd20505cee5a8179f17517f88b4c2c9e3ffdff
Alex Sandro de Almeida Souza <aalex_sandro@hotmail.com>
2022-01-30 22:25:59 -0300

    Correções de erros, melhoramentos na morte pelo spike, criação do checkpoint e implantação do pause

--- personagem.gd
+++ personagem.gd
@@ -9,30 +9,48 @@ const JUMP_DIE = 3.8
 var velocity = Vector3.ZERO
 var coin = 0
 var posicao_z
+var finalizado = false
+var checkpoint_position
+var quantidade_vidas = 3
+var start_position
+var morreu = false
 
 func _init():
 	posicao_z = get_node(".").transform.origin.z
+	checkpoint_position = $".".transform.origin
+	start_position = checkpoint_position
+	print("Vidas: ", quantidade_vidas)
 	print("Moeda: ", coin)
 
 func get_input():
+	if morreu:
+		$".".transform.origin = checkpoint_position
+		morreu = false
 	velocity.x = 0
 	velocity.z = 0
-	var right = Input.is_action_pressed("ui_right")
-	var left = Input.is_action_pressed("ui_left")
-	var jump = Input.is_action_just_pressed("ui_up")
-	
-	
-	if is_on_floor() and jump:
-		velocity.y = JUMP_SPEEDY
-	if right:
-		velocity.x += RUN_SPEEDY
-	if left:
-		velocity.x -= RUN_SPEEDY
+	if !finalizado:
+		var right = Input.is_action_pressed("ui_right")
+		var left = Input.is_action_pressed("ui_left")
+		var jump = Input.is_action_just_pressed("ui_up")
+		var pause = Input.is_action_just_pressed("ui_cancel")
 		
-	if posicao_z < get_node(".").transform.origin.z:
-		velocity.z -= VELOCITY_Z
-	if posicao_z > get_node(".").transform.origin.z:
-		velocity.z += VELOCITY_Z
+		
+		if is_on_floor() and jump:
+			velocity.y = JUMP_SPEEDY
+		if right:
+			velocity.x += RUN_SPEEDY
+		if left:
+			velocity.x -= RUN_SPEEDY
+		if pause:
+			if get_tree().paused == false:
+				get_tree().paused = true
+			else:
+				get_tree().paused = false
+			
+		if posicao_z < get_node(".").transform.origin.z:
+			velocity.z -= VELOCITY_Z
+		if posicao_z > get_node(".").transform.origin.z:
+			velocity.z += VELOCITY_Z
 		
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
@@ -48,16 +66,37 @@ func _on_morre_body_entered(body):
 	morte()
 
 func _on_mata_body_entered(body):
-	velocity.y += JUMP_DIE
-	body.morrer()
+		velocity.y += JUMP_DIE
+		body.morrer()
 
 
 func _on_leva_tiro_body_entered(body):
 	morte()
 
 func morte():
-	get_node(".").queue_free()
+	if(quantidade_vidas >= 1):
+		quantidade_vidas -= 1
+		morreu = true
+	else:
+		$".".transform.origin = start_position
+	print("Vidas: ", quantidade_vidas)
+	#get_node(".").queue_free()
 
 
 func _on_save_body_entered(body):
+	checkpoint_position = $".".transform.origin
 	body.salvar()
+
+
+func _on_finalizar_body_entered(body):
+	finalizado = true
+	body.finalizar_jogo()
+	
+func verifica_espinho(inimigo):
+	if inimigo.starts_with("spike"):
+		return true
+	return false
+
+
+func _on_spike_attack_body_entered(body):
+	morte()
